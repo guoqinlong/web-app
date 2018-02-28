c388ace5cc4f8808b3d219caf4086f25410d3aa5
Qinlong <guoqinlong@gmail.com>
2018-03-01 00:12:55 +0800

    Found the right package...

--- nioclient/src/main/java/com/qinlong/NIOClient.java
+++ nioclient/src/main/java/com/qinlong/NIOClient.java
@@ -1,14 +1,25 @@
 package com.qinlong;
 
-import org.apache.http.HttpConnection;
-import org.apache.http.impl.nio.DefaultNHttpClientConnection;
-import org.apache.http.nio.NHttpClientConnection;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import java.util.concurrent.Future;
 
 public class NIOClient {
-    public static void main(String[] args) {
-        String host = "www.baidu.com";
-        int port = 80;
-
-        NHttpClientConnection connection = new DefaultNHttpClientConnection();
+    public static void main(String[] args) throws Exception {
+        final CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault();
+        httpclient.start();
+        try {
+            final HttpGet request = new HttpGet("http://www.apache.org/");
+            final Future future = httpclient.execute(request, null);
+            final HttpResponse response = (HttpResponse) future.get();
+            System.out.println("Response: " + response.getStatusLine());
+            System.out.println("Shutting down");
+        } finally {
+            httpclient.close();
+        }
+        System.out.println("Done");
     }
 }
